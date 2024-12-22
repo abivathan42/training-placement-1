1a558d30382ae1e8ad27d3bcab5c076e87d6837e
abivathan42 <abivathan48@gmail.com>
2024-12-22 12:49:12 +0530

    Create hello.java

---
+++ 22.12.2024/hello.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public static void main(String[] args) {
+        // Complete the main method by printing the two lines below
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
